9799ef7d4a12059bbbc49e01a987af029427564a
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2023-12-15 22:11:13 +1000

    tweet API

--- apps/tweet-api/src/app/app.service.ts
+++ apps/tweet-api/src/app/app.service.ts
@@ -1,8 +1,38 @@
+import { TwitterApi } from 'twitter-api-v2';
+
 import { Injectable } from '@nestjs/common';
+import { catchError, concatMap, delay, from, map, of, toArray } from 'rxjs';
+import { TWEETS } from './tweets.const';
 
 @Injectable()
 export class AppService {
+  private twitterClient = new TwitterApi({
+    appKey: '9VYinGOTBv1IFDSP2buD98QjR',
+    appSecret: 'hZ2ghkOnGfvzA4TYZqAjXLlRLAOn5pBRbgvWAYbNviYDYUIyOP',
+    accessToken: '138187653-DLfsYdtkfbbOAamyKvVXmRbWP5NeFC5HtNwi4j8R',
+    accessSecret: '33lUc4vK7DGSUfrjR2R09Di2QuE9pIPeVQZgH3y4ZguFS',
+  });
+
   getData(): { message: string } {
-    return ({ message: 'Hello API' });
+    return { message: 'Hello API' };
+  }
+
+  tweetAllArticles(tweets: { tags: string; tweet: string }[] = TWEETS) {
+    return from(tweets).pipe(
+      concatMap((tweet) =>
+        this.tweet(`${tweet.tags} ${tweet.tweet}`).pipe(delay(5000))
+      ),
+      toArray()
+    );
+  }
+
+  tweet(tweet: string) {
+    return from(this.twitterClient.v2.tweet(tweet)).pipe(
+      map((tweet) => tweet.data),
+      catchError((error) => {
+        console.log(error.data.status);
+        return of(error.data.status);
+      })
+    );
   }
 }
---
+++ apps/tweet-api/src/app/tweets.const.ts
@@ -0,0 +1,222 @@
+export const TWEETS = [
+  {
+    tweet:
+      "I Don't Understand Cookies! Read about fundamentals of Cookies in #WebDevelopment https://javascript.plainenglish.io/i-dont-understand-cookies-ff8b7d933bb2?sk=a0f0b3a15e3ce8a702752084bf3064ba",
+    tags: '#Blog #Article #JavaScript #HTML5 #web #medium',
+  },
+  {
+    tweet:
+      'Master the HTML5 Head and Meta Tags! Read about the basics of HTML5 Head and Meta tags https://javascript.plainenglish.io/master-the-html5-head-and-meta-tags-5ae80fe4a670?sk=5fcf747bf4bc0f1944e7b6d56d99b6ac',
+    tags: '#JavaScript #HTML5 #web #medium #SEO',
+  },
+  {
+    tweet:
+      'I Promise to Make You Understand JavaScript Promises Using this Article https://blog.stackademic.com/i-promise-to-make-you-understand-javascript-promises-using-this-article-ef43b047d535?sk=c7d8f30280a4724ff5907476ee18a6af',
+    tags: '#JavaScript #HTML5 #web #medium #asynchronousprogramming',
+  },
+  {
+    tweet:
+      'Similarities and Differences between WeakMap and Map in JavaScript https://javascript.plainenglish.io/similarities-and-differences-between-weakmap-and-map-in-javascript-cfa2e8efd186?sk=1b3d2f1167bfee80a54e62d3bb2a2e62',
+    tags: '#JavaScript #HTML5 #web #medium #coding',
+  },
+  {
+    tweet:
+      'Decoding the CSS Puzzle: Mastering Specificity for Stylish Web Design https://blog.stackademic.com/decoding-the-css-puzzle-mastering-specificity-for-stylish-web-design-94d7aef509c2?sk=f854015198db652d8a8ad91f5d36e186 ',
+    tags: '#JavaScript #HTML5 #web #medium #coding #css',
+  },
+  {
+    tweet:
+      'Master JavaScript Event Propagation https://javascript.plainenglish.io/master-the-javascript-event-propagation-7080736cda6f?sk=74abee1ca6286d5f784dbd46656c412c',
+    tags: '#JavaScript #HTML5 #web #medium #coding',
+  },
+  {
+    tweet:
+      'CSS Can Impact Performance Too https://blog.stackademic.com/css-can-impact-performance-too-ec2e63f6c221',
+    tags: '#HTML5 #CSS #JavaScript #performance #coding #medium',
+  },
+  {
+    tweet:
+      'Mastering Garbage Collection in JavaScript https://blog.stackademic.com/mastering-garbage-collection-in-javascript-ca83e408b857?sk=3742a010ade1bdde80a060ea6ef17ad1',
+    tags: '#HTML5 #JavaScript #performance #coding #medium',
+  },
+  {
+    tweet:
+      'Know These Terminologies Before You Use RxJS https://javascript.plainenglish.io/know-these-terminologies-before-you-use-rxjs-70d0bdbbec20?sk=e05f820ff9b3af5000d4942f6da74e6f',
+    tags: '#medium #angular #javascript #rxjs #coding #asynchronousprogramming #medium',
+  },
+  {
+    tweet:
+      'Master the Caching in Web Development https://blog.stackademic.com/master-the-caching-in-web-development-6729295359af?sk=99c6eb3cbc34dfd9223f6394749f06bc',
+    tags: '#medium #javascript #performance #medium #webdevelopment #web',
+  },
+  {
+    tweet:
+      'A Deep Dive into PerformanceNavigationTiming Events: Enhance Web User Experience https://javascript.plainenglish.io/a-deep-dive-into-performancenavigationtiming-events-enhance-web-user-experience-8b8a7d73f4c1?sk=13bc82812b72c5d8341ed84ad669da82',
+    tags: ' #web #html5 #javascript #performance #webdevelopment #web #medium',
+  },
+  {
+    tweet:
+      'Mastering Object Referencing and Destructuring in Modern JavaScript https://javascript.plainenglish.io/mastering-object-referencing-and-destructuring-in-modern-javascript-9c3852c60237?sk=22da7635307838b021162cebf86eaf4a',
+    tags: '#HTML5 #JavaScript #medium #webdevelopment #web',
+  },
+  {
+    tweet:
+      'Optimising Website Performance: Harnessing the Power of Resource Hints https://blog.stackademic.com/optimising-website-performance-harnessing-the-power-of-resource-hints-3b10fe1868a5?sk=320fc7291e6f8d3be737fdff86f9224f ',
+    tags: '#HTML5 #medium #javascript #performance #webdevelopment #web',
+  },
+  {
+    tweet:
+      'Master the JavaScript Call Stack and Event Loop visually https://javascript.plainenglish.io/master-the-javascript-call-stack-and-event-loop-visually-02247b472fb7?sk=39f884bec9c5eadbfa89f06fb1dab979',
+    tags: '#HTML5 #medium #javascript #performance #webdevelopment #web',
+  },
+  {
+    tweet:
+      'What Happens When You Hit a Website? https://blog.stackademic.com/what-happens-when-you-hit-a-website-7ed2c2f0ebb4?sk=193ba7684c3cf71bcc4bcc17bc9f59fc',
+    tags: '#HTML5 #medium #javascript #performance #webdevelopment #web',
+  },
+  {
+    tweet:
+      'Demystifying the Rendering Pipeline and Compositing: Enhancing UI Performance https://javascript.plainenglish.io/demystifying-the-rendering-pipeline-and-compositing-enhancing-ui-performance-15703fa74326?sk=ee0b1ee852634521d48fd10a336b8f34',
+    tags: '#HTML5 #medium #javascript #performance #webdevelopment #web',
+  },
+  {
+    tweet:
+      'What Does It Take to Be a Good Web Developer? https://blog.stackademic.com/what-it-takes-to-be-a-good-web-developer-9b047ae6eb98?sk=d40ac2ef756c5067e9b5243414e5d986',
+    tags: '#HTML5 #medium #javascript #performance #webdevelopment #web #interview',
+  },
+  {
+    tweet:
+      'Master the async and defer Scripts in HTML https://javascript.plainenglish.io/master-the-async-and-defer-scripts-in-html-1d3050ddcfaa?sk=e1ba2af21862e6aea9418b7029d19098',
+    tags: '#JavaScript #HTML5 #Performance #medium #webdevelopment #web',
+  },
+  {
+    tweet:
+      'The Rust Programming Language  https://medium.com/rustaceans/the-rust-programming-language-4b22bc717ecc',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Introduction  https://medium.com/rustaceans/the-rust-programming-language-introduction-b173fdc2e340',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Primitives — String  https://medium.com/rustaceans/the-rust-programming-language-primitives-string-d7d053bfe0c8?sk=8ee05816d1a9ed5c68a2509a875271a1',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Primitives — Floats and Immutability https://medium.com/rustaceans/the-rust-programming-language-primitives-floats-9b8bdc11e453?sk=d2f658e79529ccf0de17ac9e86d08fc0 ',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Primitives — Numeric Types and Type Annotations  https://medium.com/rustaceans/the-rust-programming-language-primitives-numeric-types-and-type-annotations-c171f37e32dc?sk=ea99bbd71cc06766b371c5c9fee453a9',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Primitives — Float sizes Integers  https://medium.com/rustaceans/the-rust-programming-language-primitives-float-sizes-integers-164e7a061b54',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Primitives — Booleans, Conditionals, Statements and Expressions https://medium.com/rustaceans/the-rust-programming-language-primitives-booleans-conditionals-statements-and-expressions-68010fa09280?sk=e7e9bfb108c12e790c2503e0d3dd00df',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Primitives — Summary  https://medium.com/rustaceans/the-rust-programming-language-primitives-summary-14da18b6d54e?sk=7ee07c8208a5e7dfe91f6d302af20828',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Collections — Tuples #Rust Medium https://medium.com/@ankitbtanna/the-rust-programming-language-collections-tuples-557f908c2dda',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Collections — Structs  https://medium.com/@ankitbtanna/the-rust-programming-language-collections-structs-1a20f82cb02c?sk=625fed2eaf43ede7f741e01f711aa4ce',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Collections — Arrays  https://medium.com/@ankitbtanna/the-rust-programming-language-collections-arrays-13c07bc5d58d?sk=a03006ed77a1057d325cfbaf6f3b4e12',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Collections — Memory  https://medium.com/@ankitbtanna/the-rust-programming-language-collections-memory-8e7de166b27b?sk=befb90ab731d31a213f559319a72e26e',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Collections — Summary #Medium #Rust https://medium.com/@ankitbtanna/the-rust-programming-language-collections-summary-1269a92dbfcc',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Pattern Matching — Enums #Medium #Rust https://blog.stackademic.com/the-rust-programming-language-pattern-matching-enums-dde03e1761d7?sk=f2ff0ada98411fb9ac6d7509ff634197',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Pattern Matching — Pattern matching using Enums  https://medium.com/@ankitbtanna/the-rust-programming-language-pattern-matching-pattern-matching-using-enums-a454b5f0ffc7?sk=78ce798c6dd1647664ebc5dd332843da',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Pattern Matching — Methods  https://medium.com/@ankitbtanna/the-rust-programming-language-pattern-matching-methods-c6902877b714?sk=972b79fe27f276b00a47825ef11165f4',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Pattern Matching — Type Parameters  https://medium.com/@ankitbtanna/the-rust-programming-language-pattern-matching-type-parameters-4a239c45df9f?sk=171da69c88c574fe9d7918ddb1a29e26',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Pattern Matching — Summary  https://medium.com/@ankitbtanna/the-rust-programming-language-pattern-matching-summary-654e7c3067aa?sk=353fe8b9ff34def3ad9d9250c8646f3d',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Vectors — Vectors  https://medium.com/@ankitbtanna/the-rust-programming-language-vectors-vectors-02c322363cc2?sk=22e6d6fe3d4fe5aaa7e730ecc871ed8d',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Vectors — Stack Memory vs Heap Memory  https://medium.com/@ankitbtanna/the-rust-programming-language-vectors-stack-memory-vs-heap-memory-459a06270d97?sk=6e6bd8be0fdc9ee5d7510596953e60f6',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Vectors — Summary  https://medium.com/@ankitbtanna/the-rust-programming-language-vectors-summary-00ea57f05ffe?sk=a87c4fe52833f89dd34d25f186c39b5e',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Ownership — Memory Management of Rust  https://medium.com/@ankitbtanna/the-rust-programming-language-ownership-heap-bookkeeping-and-memory-management-of-rust-daeac574208b?sk=6f3406ce1b6697f31c6a85c3e8224252',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — References and Borrows — Understanding Values by Reference and Borrows  https://medium.com/@ankitbtanna/the-rust-programming-language-references-and-borrows-understanding-values-by-reference-and-9ecac9e99f52?sk=356ec1bbb9baa24618f0299f4d118c6b',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — References and Borrows — Mutable References #Medium #Rust https://medium.com/@ankitbtanna/the-rust-programming-language-references-and-borrows-mutable-references-bd7e11b2dc37?sk=6d72ba74c20c10ef09a5e22724d1ed62',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — References and Borrowing — Slices  https://medium.com/@ankitbtanna/the-rust-programming-language-references-and-borrowing-slices-cd58d10443d7?sk=acc6578640dcb7d815ea1e29d78c40da',
+    tags: '#Rust #Medium',
+  },
+  {
+    tweet:
+      'The Rust Programming Language — Lifetime — Lifetime, Lifetime Annotations, Lifetime Elision and Static Lifetime  https://medium.com/@ankitbtanna/the-rust-programming-language-lifetime-lifetime-lifetime-annotations-lifetime-elision-and-7222339f59e5?sk=8237e3645d66477e94ac03b97c2d4638',
+    tags: '#Rust #Medium',
+  },
+];
